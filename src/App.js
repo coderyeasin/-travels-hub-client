13f7f767243e4c1acaf679d1a23dd9b3c5cd7ecc
coderyeasin <yeasin3233@gmail.com>
2021-10-29 00:26:13 +0600

    add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Home/Header/Header';
+import NotFound from './Pages/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/"><Home></Home></Route>
+          <Route path="/home"><Home></Home></Route>
+          
+          <Route path="*"><NotFound></NotFound> </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
